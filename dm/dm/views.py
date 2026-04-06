b46210638c5e625a17be0e2f58a56b10a6ed9c91
Bellamy-spec <459206164@qq.com>
2026-04-06 19:55:00 +0800

    完善走读标记功能

--- dm/dm/views.py
+++ dm/dm/views.py
@@ -2324,14 +2324,9 @@ def sep_change_info(request, student_id):
                     'title': title,
                 })
 
-            print(change.no_dorm)
-
-            # 走读勾选的情况
-            if change.no_dorm:
-                # 将宿舍号设为000，床铺号设为-1
-                student.dorm = '000'
-                student.bed = -1
-            else:
+            # print(change.dorm)
+            # 走读未勾选的情况
+            if not change.no_dorm:
                 # 验证床号
                 if change.bed < 1 or change.bed > 8:
                     return render_ht(request, 'sep_change_info.html', context={
@@ -2342,15 +2337,13 @@ def sep_change_info(request, student_id):
                         'title': title,
                     })
 
-                # 宿舍号和床铺号设为正常值
-                student.dorm = change.dorm
-                student.bed = change.bed
-
             # 修改新生信息
             student.name = change.name
             student.id_number = change.id_number
             student.gender = change.gender
             student.cs = change.cs
+            student.dorm = change.dorm
+            student.bed = change.bed
             student.fill_blank()
             student.save()
 
@@ -2416,12 +2409,8 @@ def sep_add_one(request, year):
                     'title': title,
                 })
 
-            # 走读勾选的情况
-            if new_st.no_dorm:
-                # 将宿舍号设为000，床铺号设为-1
-                new_st.dorm = '000'
-                new_st.bed = -1
-            else:
+            # 走读未勾选的情况
+            if not new_st.no_dorm:
                 # 验证床铺号
                 if new_st.bed < 1 or new_st.bed > 8:
                     return render_ht(request, 'sep_add_one.html', context={
